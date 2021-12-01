19e1b9b4e08abcc4da79b6a316dbbb0e33a06f8d
Scott Anderson <scottkenanderson@goolemail.com>
2021-12-01 22:23:01 +0000

    fix part 1

--- 2021-12-01/src/part_1.rb
+++ 2021-12-01/src/part_1.rb
@@ -9,16 +9,18 @@ counter = 0
 puts filename
 
 lines = File.readlines(filename)
-next_line = lines.shift
+next_line = lines.shift.to_i
+puts "#{next_line} (N/A)"
 
 lines.each do |f|
-  puts "old: #{next_line}"
-  puts "new: #{f}"
-  if f > next_line
+  f_int = f.to_i
+  if f_int > next_line
     counter += 1
-    puts "counter incremented"
+    puts "#{f_int} (increased)"
+  else
+    puts "#{f_int} (decreased)"
   end
-  next_line = f
+  next_line = f_int
 end
 
 puts "answer is #{counter}"

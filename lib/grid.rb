5c43a69690a6284920a463c426c8ac92a2ba135e
Scott <scottkenanderson@googlemail.com>
2021-12-13 00:38:06 +0000

    add day 11 (#17)

---
+++ lib/grid.rb
@@ -0,0 +1,26 @@
+class Grid
+  def initialize(initial_grid)
+    @grid = initial_grid
+  end
+
+  def valid_location?(point)
+    point.x >= 0 && point.y >= 0 && point.x < @grid[0].length && point.y < @grid.length
+  end
+
+  def get_neighbours(point)
+    i = point.x
+    j = point.y
+    neighbours = []
+    (i - 1..i + 1).each do |i_inner|
+      (j - 1..j + 1).each do |j_inner|
+        neighbouring_point = Point.new(i_inner, j_inner)
+        neighbours << neighbouring_point if neighbouring_point != point && valid_location?(neighbouring_point)
+      end
+    end
+    neighbours
+  end
+
+  def to_s
+    @grid.map { |y| y.map(&:to_s).join(" ") }.join("\n")
+  end
+end

a93b13f40ea764278e28c2bab5c5d7ba68b0aa4d
Scott Anderson <scottkenanderson@googlemail.com>
2021-12-19 20:03:00 +0000

    finish day16

--- 2021-12-16/src/main.rb
+++ 2021-12-16/src/main.rb
@@ -4,17 +4,21 @@ require "set"
 require_relative "../../lib/read_file"
 require_relative "./packet"
 
-def part_1
+def get_all_sub_packets(all_packets, packet)
+  all_packets << packet
+  packet.sub_packets.each do |p|
+    get_all_sub_packets(all_packets, p)
+  end
 end
 
-def part_2(polymer_template, pair_insertion_rules)
-  polymer = Polymer.new polymer_template, pair_insertion_rules
-  40.times do |i|
-    polymer.step_efficient
-    puts "#{i + 1}: #{polymer.length}"
-  end
-  puts polymer
-  puts (polymer.most_common - polymer.least_common).to_s
+def part_1(packet)
+  all_packets = []
+  get_all_sub_packets(all_packets, packet)
+  puts ["sum of versions: ", all_packets.map(&:version_number).sum].join
+end
+
+def part_2(packet)
+  puts ["packet value: ", packet.value].join
 end
 
 def parse_literal_value_packet(transmission_bits, packet)
@@ -25,57 +29,44 @@ def parse_literal_value_packet(transmission_bits, packet)
     4.times do
       number << transmission_bits.shift
     end
-    puts number.join
+    # puts ["literal value packet number:", number.join, number.join.to_i(2)].join(" ")
     packet.add_number(number)
   end
-
   packet
 end
 
 def parse_operator_packet(transmission_bits, packet)
   packet.set_length_type(transmission_bits.shift)
-  puts [packet.length_bits, "length bits", packet.length_type]
   length = []
   packet.length_bits.times do
     length << transmission_bits.shift
   end
-  puts packet
-  puts "length"
-  puts length.join.to_i(2)
   if packet.length_bits == 11
     length.join.to_i(2).times do
       sub_packet = parse_header(transmission_bits)
-      if packet.is_literal_value
-        puts "literally"
-        parse_literal_value_packet(transmission_bits, packet)
+      if sub_packet.is_literal_value
+        parse_literal_value_packet(transmission_bits, sub_packet)
       else
-        parse_operator_packet(transmission_bits, packet)
+        parse_operator_packet(transmission_bits, sub_packet)
       end
       packet.add_sub_packet(sub_packet)
-      puts sub_packet.numbers
     end
   end
   if packet.length_bits == 15
     sub_transmission = []
-    puts length.join.to_i(2)
-    puts length.join.to_i(2)
     length.join.to_i(2).times do
       sub_transmission << transmission_bits.shift
     end
-    puts sub_transmission.join
     until sub_transmission.length == 0
       sub_packet = parse_header(sub_transmission)
       if sub_packet.is_literal_value
-        puts "literally"
         parse_literal_value_packet(sub_transmission, sub_packet)
       else
         parse_operator_packet(sub_transmission, sub_packet)
       end
       packet.add_sub_packet(sub_packet)
-      puts "numbers", sub_packet.numbers
     end
   end
-  puts "sub packet length", packet.sub_packets.length
 end
 
 def parse_header(transmission_bits)
@@ -94,32 +85,17 @@ def parse_input(input)
   transmission = input.shift.strip
   puts transmission
   transmission_bits = transmission.hex.to_s(2).rjust(transmission.size * 4, "0").chars
-  puts transmission_bits.length
-  puts transmission_bits.join("")
   packet = parse_header(transmission_bits)
 
-  puts [packet.version_number, packet.version, "version number"]
-
   if packet.is_literal_value
-    puts "literally"
     parse_literal_value_packet(transmission_bits, packet)
   else
     parse_operator_packet(transmission_bits, packet)
   end
-  puts packet
-
-  # (4 - packet.length % 4).times do
-  #   transmission_bits.shift
-  # end
-  # puts transmission_bits.length
-  exit
+  packet
 end
 
 input = read_file
-polymer_template, pair_insertion_rules = parse_input(input)
-
-puts polymer_template
-puts pair_insertion_rules
-
-part_1(polymer_template, pair_insertion_rules)
-part_2(polymer_template, pair_insertion_rules)
+packet = parse_input(input)
+part_1(packet)
+part_2(packet)
--- 2021-12-16/src/packet.rb
+++ 2021-12-16/src/packet.rb
@@ -16,7 +16,7 @@ class Packet
   end
 
   def is_literal_value
-    @type.to_i(2).to_s(16) == "4"
+    calculate_type == "LITERAL"
   end
 
   def add_number(number)
@@ -33,6 +33,10 @@ class Packet
 
   def version_number
     @version.to_i(2)
+  end
+
+  def type_number
+    @type.to_i(2)
   end
 
   def length_bits
@@ -54,7 +58,47 @@ class Packet
     6 + (5 * @numbers.length)
   end
 
+  def value
+    return @numbers.join.to_i(2) if calculate_type == "LITERAL"
+    if sub_packets.empty?
+      return "no sub packets"
+    end
+    sub_packet_values = @sub_packets.map(&:value)
+    case calculate_type
+    when "LESS THAN"
+      sub_packet_values[0] < sub_packet_values[1] ? 1 : 0
+    when "GREATER THAN"
+      sub_packet_values[0] > sub_packet_values[1] ? 1 : 0
+    when "EQUAL TO"
+      sub_packet_values[0] == sub_packet_values[1] ? 1 : 0
+    when "SUM"
+      sub_packet_values.sum
+    when "PRODUCT"
+      sub_packet_values.reduce(&:*)
+    when "MINIMUM"
+      sub_packet_values.min
+    when "MAXIMUM"
+      sub_packet_values.max
+    else
+      puts "unknown type: #{calculate_type}"
+      "?? ?"
+    end
+  end
+
+  def calculate_type
+    return "SUM" if type_number == 0
+    return "PRODUCT" if type_number == 1
+    return "MINIMUM" if type_number == 2
+    return "MAXIMUM" if type_number == 3
+    return "LITERAL" if type_number == 4
+    return "GREATER THAN" if type_number == 5
+    return "LESS THAN" if type_number == 6
+    return "EQUAL TO" if type_number == 7
+  end
+
   def to_s
-    "packet: V: #{version_number}\tType: #{@type}\nNumbers: #{@numbers.flatten.join.to_i(2)}"
+    "packet: V: #{version_number}\tType: #{type_number}, #{calculate_type}" \
+    "\nNumbers: #{@numbers.flatten.join}: #{@numbers.flatten.join.to_i(2)}, Value: #{value}\n" \
+    "subpackets: #{@sub_packets.join("\n")}"
   end
 end
